7e008ece850e64dcba4978f9c37cc1aa3155e118
Dhyaneshwari Bhende <110776460+DhyaneshwariBhende@users.noreply.github.com>
2023-02-21 18:01:46 +0530

    Add files via upload

---
+++ chessBoard.cpp
@@ -0,0 +1,25 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    
+
+    string s; cin>>s;
+
+        int i=s[0]= 'a';
+        i++;
+        int j=(s[1]='0');
+
+        if( (i+j)%2==0 ){
+            cout<<"Black";
+        }
+        else{
+            cout<<"White";
+        }
+    return 0;
+}
